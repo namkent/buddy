df81043619dae0840ca461636ef993caaa253440
namkent <namkent@gmail.com>
2026-04-18 13:20:34 +0700

    update

--- test_mem0.py
+++
@@ -1,4 +0,0 @@
-import requests
-res = requests.post("http://localhost:8000/search", json={"query": "hello", "user_id": "test_user", "top_k": 5})
-print(res.status_code)
-print(res.text)
--- test_mem0_add.py
+++
@@ -1,4 +0,0 @@
-import requests
-res = requests.post("http://localhost:8000/memories", json={"messages": [{"role": "user", "content": "I like apple"}], "user_id": "test_user"})
-print(res.status_code)
-print(res.text)
